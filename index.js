3439c7c979cc5fcdeb58de55e2e2c6a2e01e9522
prince <princefarron@gmail.com>
2024-09-04 16:11:27 +0100

    Add CORS

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 const cors = require('cors')
-)
 const port=process.env.PORT
 const characters=require("./harrypotter.json")
 app.use(express.json())
